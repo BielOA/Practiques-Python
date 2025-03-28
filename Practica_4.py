861742fe67c63ff2cd2131cb8ebc3b303a5a5b4d
BielOA <biel.ortega@inscanroca.com>
2025-03-28 19:27:55 +0100

    P4 part 2

--- Practica_4.py
+++ Practica_4.py
@@ -1,10 +1,10 @@
+def valors_max(num1, num2, num3):
+    return max(num1, num2, num3)
+
 num1 = int(input("Introdueix un valor: "))
 num2 = int(input("Introdueix un segon valor: "))
 num3 = int(input("Introdueix un tercer valor: "))
 
-if num1 > num2 and num3:
-    print(f"{num1} és el valor més gran")
-elif num2 > num1 and num3:
-    print(f"{num2} és el valor més gran")
-elif num3 > num1 and num3:
-    print(f"{num3} és el valor més gran")
+max_valor = valors_max(num1, num2, num3)
+
+print(f"El valor més alt és {max_valor}")
