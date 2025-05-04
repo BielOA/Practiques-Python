7e85d504ed84c6c674be7da17fff7840b436dc69
BielOA <biel.ortega@inscanroca.com>
2025-05-04 12:17:19 +0200

    cnv pr6

--- Practica_6.py
+++ Practica_6.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def notes_alumnes(Notes_alumnes_practica6):
     try:
         alumnes = pd.read_excel(Notes_alumnes_practica6)
-        notes = [c for c in alumnes.columns if c.startswith('Nota')]
+        notes = [col for col in alumnes.columns if col.startswith('Nota')]
         alumnes['Mitjana'] = alumnes[notes].mean(axis=1).round(2)
         return alumnes
     except Exception as e:
@@ -20,18 +20,38 @@ def qual_quant(m):
     else: return "Assoliment Excel·lent"
 
 def histograma(alumnes):
-    comptatge = alumnes['Qualificació'].value_counts().reindex(
+    contar = alumnes['Qualificació'].value_counts().reindex(
         ["No assoliment", "Assoliment Satisfactori", "Assoliment Notable", "Assoliment Excel·lent"],
         fill_value=0
     )
     colors = ["#f03629", "#f0da16", "#a7f016", "#41f011"]
 
-    plt.bar(comptatge.index, comptatge.values, color=colors, width=0.6)
+    plt.bar(contar.index, contar.values, color=colors, width=0.6)
     plt.title ("Qualificacions de la classe")
     plt.xlabel("Qualificació")
     plt.ylabel("Nombre d'alumnes")
     plt.xticks(rotation=10)
     plt.savefig('histograma_qualificacions.png')
+    plt.close()
+
+def mill_pitj(alumnes):
+    notes_cols = [col for col in alumnes.columns if col.startswith('Nota')]
+
+    millor = alumnes.loc[alumnes['Mitjana'].idxmax()]
+    pitjor = alumnes.loc[alumnes['Mitjana'].idxmin()]
+
+    plt.plot(range(1, 7), millor[notes_cols], label=f"Millor: {millor['Alumnes']}", marker='o', color='green')
+    plt.plot(range(1, 7), pitjor[notes_cols], label=f"Pitjor: {pitjor['Alumnes']}", marker='o', color='red')
+
+    plt.title("Evolució de les notes del millor i pitjor alumne")
+    plt.xlabel("Avaluació")
+    plt.ylabel("Nº nota")
+    plt.xticks(range(1, 7), notes_cols)
+    plt.ylim(0, 11)
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig('evolucio_millor_pitjor.png')
     plt.close()
 
 arxiu = "Notes_alumnes_practica6.xlsx"
@@ -40,6 +60,8 @@ alumnes = notes_alumnes(arxiu)
 if not alumnes.empty:
     alumnes['Qualificació'] = alumnes['Mitjana'].apply(qual_quant)
     histograma(alumnes)
-    print("El grafic s'ha desat com 'histograma_qualificacions.png'")
+    print("El grafic 'Qualificacions de la classe' s'ha desat com 'histograma_qualificacions.png'")
+    mill_pitj(alumnes)
+    print("El grafic 'Evolució de les notes del millor i pitjor alumne' s'ha desat com 'evolucio_millor_pitjor.png'")
 else:
     print("No s'han pogut carregar les dades.")
